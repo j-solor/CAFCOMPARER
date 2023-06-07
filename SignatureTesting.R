93c7e67f8e877951b9abca4833cdeb38766d67e7
margaux0400 <margaux.dore@univ-tlse3.fr>
2023-06-07 13:43:36 +0200

    Add annotation in SignatureTesting.R

--- SignatureTesting.R
+++ SignatureTesting.R
@@ -5,12 +5,16 @@ library(org.Hs.eg.db)
 library(readxl)
 library(GSVA)
 library(pheatmap)
+
+library(biomaRt)
+
 ################################################################################
 sample_ID = "private" # private | public
 signature_selection = NULL  # Luo_NatCom2022 | Foster_CancerCell2022 | Huang_CancerCell2022 | Verginadis_NatureCellBio2022 | Grauel_NatComms2020 | Carpenter_CancerDiscovery2023
 include_tech_annot = FALSE # TRUE | FALSE
 whatever_genes_filename = "Receptors_HGNC.csv" # Receptors_info.csv "whatever csv ythat uses tab as separator and has columns of groups (avoid spaces in names)
 ################################################################################
+
 #' Filter a dataframe to keep genes with at least a defined % of non 0 expression samples
 #'@description
 #' `Exclude_0s` requires a data.frame with genes as rows,
@@ -39,10 +43,10 @@ ID_converter <- function(df, # dataset with ProbeIDs as rownames
   final_df <- merge(df,annotation_table, by.x=0, by.y=old_IDs)
   
   # Stats of conversion
-  non_agg <- nrow(final_df)
-  non_agg_uniq <- length(unique(final_df[new_IDs]))
-  non_agg_nas <- sum(is.na(final_df[new_IDs]))
-  non_agg_nonas <- non_agg-non_agg_nas
+  non_agg <- nrow(final_df) # nb of genes 
+  non_agg_uniq <- length(unique(final_df[new_IDs])) # nb of unique 
+  non_agg_nas <- sum(is.na(final_df[new_IDs])) # nb of gene with no new_ID 
+  non_agg_nonas <- non_agg-non_agg_nas # 
   
   # Conversion
   final_df <- aggregate(final_df,
@@ -65,26 +69,25 @@ ID_converter <- function(df, # dataset with ProbeIDs as rownames
   return(final_df)
 }
 ################################################################################
-setwd("/home/jacobo/Documents/02_TRANSDUCER/04_Deconvolution_TE/03_Sigantures/cell_types/fibroblast/PJ2003085/260523_CAFComparer/")
+setwd("/home/margaux.dore/Documents/CAFCOMPARER")
 
 # Data loading
 ## CAF
-acafs.info <- read_tsv("data/CAF_metadata.tsv")
-acafs.raw <- read_tsv("data/CAF_rawcounts.tsv") %>% 
+acafs.info <- read_tsv("data/CAF_metadata.tsv") # metadata about CAFs 
+acafs.raw <- read_tsv("data/CAF_rawcounts.tsv") %>% # table de comptage
   dplyr::select(EnsemblID, acafs.info$rawID) %>%
   dplyr::relocate(EnsemblID, acafs.info$rawID)
 
-
 ## Signatures
-if (is.null(signature_selection)){
+if (is.null(signature_selection)){ #initialize at 
   signatures = tibble(signature = character(), value = character())
-  sign_list <- excel_sheets("data/CAF_signatures.xlsx")
+  sign_list <- excel_sheets("data/CAF_signatures.xlsx") # list of sheet's names
 for (sign in sign_list){
   signatures_temp <- read_xlsx("data/CAF_signatures.xlsx", sheet = sign) %>%
     rename_with( ~ paste0(sign,"_", .x)) %>%
     pivot_longer(cols = everything(), names_to = "signature")
   
-  signatures <- bind_rows(signatures, signatures_temp) 
+  signatures <- bind_rows(signatures, signatures_temp) # table with gene name in each sheet
 }
 
 }else {
@@ -93,7 +96,7 @@ for (sign in sign_list){
   
 }
 
-list_of_signatures <- split(signatures, f = signatures$signature) %>%
+list_of_signatures <- split(signatures, f = signatures$signature) %>% # list of list of gene names in each sheet
   map(~ .$value)
 
 # Preprocessing
@@ -107,29 +110,30 @@ if (sample_ID == "public") {
   names(acafs.raw)[-1] <-  acafs.info$Name
   acafs.info <- column_to_rownames(acafs.info, "Name")
 }
-acafs.raw <-column_to_rownames(acafs.raw, "EnsemblID")
+
+acafs.raw <-column_to_rownames(acafs.raw, "EnsemblID") # table de comptage (EnsemblID)
 
 ## Normalize
-acafs.tmm <- Exclude_0s(acafs.raw, 0.5) %>% DGEList() %>% 
+acafs.tmm <- Exclude_0s(acafs.raw, 0.5) %>% DGEList() %>% # normalized count table
   calcNormFactors(method = "TMM") %>% cpm() 
 
 ## convert ID
-annot <- AnnotationDbi::select(org.Hs.eg.db, keys=rownames(acafs.tmm), columns="SYMBOL", keytype="ENSEMBL")
+annot <- AnnotationDbi::select(org.Hs.eg.db, keys=rownames(acafs.tmm), columns="SYMBOL", keytype="ENSEMBL") # Annotation table
 cafs.choose.sym <- ID_converter(df = acafs.tmm,annotation_table = annot,
                                 old_IDs = "ENSEMBL", new_IDs = "SYMBOL")
 
 # Analysis
 ## Barplot of whatever gene
-gene = "SHMT1"
+gene = "COL1A1"
 cafs.gene.expression <- as_tibble(cafs.choose.sym, rownames = "gene") %>% 
   pivot_longer(cols = -gene ,names_to = "CL",values_to = "expression" ) %>%
   pivot_wider(id_cols = "CL", names_from = "gene", values_from = "expression")
 
 dplyr::mutate(cafs.gene.expression, CL = fct(CL, levels = arrange(cafs.gene.expression, desc(get(gene)))$CL)) %>%
   ggplot(aes(x = get(gene), y = CL)) + 
-  #aes_string(x = gene, y = "CL") +
-  geom_bar(stat = "identity")
-
+  geom_bar(stat = "identity") +
+  labs(x = "COL1A1", y = "CAFs") +
+  theme_bw()
 
 ## GSEA of signatures
 gsvaRes <- gsva(cafs.choose.sym %>% data.matrix(), list_of_signatures)
@@ -141,17 +145,18 @@ if (include_tech_annot == TRUE){
                     "treatment_neoadjuvant", "OS")
   tech_annot = acafs.info[full_colanot]
   
-}else {tech_annot = NULL}
+} else {tech_annot = NULL}
 
 
 pheatmap(gsvaRes,
          cluster_cols = T, cluster_rows = T, annotation_col = tech_annot)
 
-
 ### Specific pheatmaps per signature set (output in files)
+
+#sign = "Foster_CancerCell2022"
 for (sign in sign_list){
-  sign_sub <- dplyr::filter(signatures, str_detect(signature, sign)) %>%
-    dplyr::filter(value %in% rownames(cafs.choose.sym)) %>% 
+  sign_sub <- dplyr::filter(signatures, str_detect(signature, sign)) %>% # filter the signatures == sign
+    dplyr::filter(value %in% rownames(cafs.choose.sym)) %>% # only keep the values present in cafs.choose.sym
     group_by(value) %>% 
     mutate(signature = ifelse(base::duplicated(value,fromLast=T), "multiple", signature)) %>%
     distinct(value, .keep_all = T,) %>%
