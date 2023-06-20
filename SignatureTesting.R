b57c504e42fe84236fdf695aff67fd677def3bac
j-solor <jacobo.solorzano@inserm.fr>
2023-06-20 12:22:22 +0200

    Correction whatever_gene_function function aesthetics

--- SignatureTesting.R
+++ SignatureTesting.R
@@ -224,6 +224,7 @@ ggplot(tb_organized, aes(x=CL, y=value, fill=Signature)) +
 
 whatever_genes_function <- function(file_path, expression_table, scale_option, output_file) {
   
+  # Main
   whatever_genes <- read_tsv(file_path) %>%
     pivot_longer(cols = everything(), names_to = "signature") %>%
     dplyr::filter(!is.na(value))
@@ -231,29 +232,37 @@ whatever_genes_function <- function(file_path, expression_table, scale_option, o
   list_of_whatever_genes <- split(whatever_genes, f = whatever_genes$signature) %>%
     map(~ .$value)
   
-  gsvaRes_whatever_genes <- gsva(data.matrix(expression_table), list_of_whatever_genes)
+  gsvaRes <- gsva(data.matrix(expression_table), list_of_whatever_genes)
   
+  # Plots
+  ## Heatmap
   gene_anotation <- dplyr::filter(whatever_genes, value %in% rownames(expression_table)) %>% 
     group_by(value) %>% 
-    mutate(signature = ifelse(base::duplicated(value,fromLast=T), "multiple", signature)) %>%
+    mutate(signature = ifelse(base::duplicated(value,fromLast=T),
+                              "multiple", signature)) %>%
     distinct(value, .keep_all = T,) %>%
     dplyr::arrange(signature) %>%
     column_to_rownames("value")
   
   expression_table[rownames(gene_anotation),] %>% 
     pheatmap(cellwidth=15, cellheight=15, filename = output_file,
-             cluster_cols = T, cluster_rows = F, scale = scale_option, show_rownames = T, annotation_col = as.data.frame(t(gsvaRes_whatever_genes)),
+             cluster_cols = T, cluster_rows = F, scale = scale_option,
+             show_rownames = T, annotation_col = as.data.frame(t(gsvaRes)),
              annotation_row = gene_anotation)
   
+  ## Boxplot
   expression_table_boxplot <- as_tibble(expression_table, rownames = "gene") %>% # tb_cafs_GE
     pivot_longer(cols = -gene , names_to = "CL",values_to = "value" )
   
-  whatever_genes_boxplot <- left_join(whatever_genes, expression_table_boxplot, by = c("value" = "gene")) %>% 
+  whatever_genes_boxplot <- left_join(whatever_genes, expression_table_boxplot, 
+                                      by = c("value" = "gene")) %>% 
     dplyr::rename(gene = value, value = value.y, Signature = signature) %>%
     dplyr::filter(!is.na(CL))
   
-  levels <- group_by(whatever_genes_boxplot, CL) %>% summarise(mean=mean(value)) %>% arrange(dplyr::desc(mean))
-  whatever_genes_organized <- whatever_genes_boxplot %>% dplyr::mutate(CL = fct(CL, levels = levels$CL)) # tb_organized
+  levels <- group_by(whatever_genes_boxplot, CL) %>% summarise(mean=mean(value)) %>%
+    arrange(dplyr::desc(mean))
+  whatever_genes_organized <- whatever_genes_boxplot %>%
+    dplyr::mutate(CL = fct(CL, levels = levels$CL)) # tb_organized
   
   dev.off()
   
@@ -269,7 +278,8 @@ whatever_genes_function <- function(file_path, expression_table, scale_option, o
   missing_genes <- setdiff(genes, heatmap_genes)
   
   if(length(missing_genes) > 0) {
-    cat("The following genes were not in the expression table :", paste(missing_genes, collapse = ", "))
+    cat("The following genes were not in the expression table :",
+        paste(missing_genes, collapse = ", "))
   }
   
 }
